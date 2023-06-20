a6a7351dfc04ddd799c9ea3085e66435b9c1bbf7
Sandro Soria <84270313+Sandro642@users.noreply.github.com>
2023-06-20 18:14:38 +0200

    Update GuiAPI.java

--- src/main/java/fr/sandro642/github/GuiAPI/GuiAPI.java
+++ src/main/java/fr/sandro642/github/GuiAPI/GuiAPI.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.Bukkit;
 
 public class GuiAPI {
 
